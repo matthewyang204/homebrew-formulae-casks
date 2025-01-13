32867770b053a007a72340700d0ed63002c45528
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2025-01-13 09:32:13 -0800

    Add Notepad==

---
+++ notepad==.rb
@@ -0,0 +1,20 @@
+cask "notepad==" do
+  version "4.8.1"
+  sha256 :no_check
+
+  url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-x86_64.zip"
+  name "Notepad=="
+  desc "Simple Windows Notepad replacement for Unix systems"
+  homepage "https://github.com/matthewyang204/NotepadEE"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Notepad==.app"
+
+  zap trash: [
+    "~/.notepadee",
+  ]
+end
