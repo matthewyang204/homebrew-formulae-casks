0b3cc0748f3210aac844b6c1a642d24adf47a644
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2025-03-11 12:43:02 -0700

    Set build and install method

--- Formula/notepadee-source-macos.rb
+++ Formula/notepadee-source-macos.rb
@@ -20,8 +20,7 @@ class notepadee-source-macos < Formula
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cd macOS", "&&", "./autogen.sh", "&&", "sudo make install"
   end
 
   test do
