85b7aaa1f3f8380a0509d37e5872206486057a35
Matthew Yang <matthew.yang204@gmail.com>
2025-03-18 20:59:35 -0700

    5.0.3, not 5.0.23!

--- Casks/notepadee-arm64.rb
+++ Casks/notepadee-arm64.rb
@@ -1,5 +1,5 @@
 cask "notepadee-arm64" do
-  version "5.0.23"
+  version "5.0.3"
   sha256 :no_check
 
   url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-arm64.zip"
