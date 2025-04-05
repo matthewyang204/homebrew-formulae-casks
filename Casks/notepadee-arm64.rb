e866ce3d661c62de4c2aad367907224e8e3a19a4
Matthew Yang <matthew.yang204@gmail.com>
2025-04-04 19:34:38 -0700

    Bump Notepad== cask version numbers

--- Casks/notepadee-arm64.rb
+++ Casks/notepadee-arm64.rb
@@ -1,5 +1,5 @@
 cask "notepadee-arm64" do
-  version "5.0.6"
+  version "5.0.7"
   sha256 :no_check
 
   url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-arm64.zip"
