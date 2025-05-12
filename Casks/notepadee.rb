ea0764b2fcf5790a0fb41efc0d254877f335f725
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2025-05-12 13:40:58 -0700

    Dynamically download binaries depending on architecture

--- Casks/notepadee.rb
+++ Casks/notepadee.rb
@@ -1,8 +1,10 @@
 cask "notepadee" do
+  arch arm: "arm64", intel: "x86_64"
+  
   version "5.0.10"
   sha256 :no_check
 
-  url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-x86_64.zip"
+  url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-#{arch}.zip"
   name "Notepad=="
   desc "Simple Windows Notepad replacement for Unix systems"
   homepage "https://github.com/matthewyang204/NotepadEE"
