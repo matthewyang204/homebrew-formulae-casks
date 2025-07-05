b52ade4ebab15b1c9ded5b40c50206c8af708c0e
Matthew Yang <matthew.yang204@gmail.com>
2025-07-05 13:04:24 -0700

    don't check sha256 or formula will break

--- Formula/dproc.rb
+++ Formula/dproc.rb
@@ -1,6 +1,6 @@
 class Dproc < Formula
     version "1.0.1"
-    sha256 "f41de0f1df46dc62afd837d5b500d052dc4315c220b3db776b7a7d1c6c93992d"
+    sha256 :no_check
 
     desc "A basic CLI data processor, designed to be fed data and output data directly from the commandline"
     homepage "https://github.com/matthewyang204/dproc"
