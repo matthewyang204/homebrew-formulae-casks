570caad30ac4fc3113493ede5de1b821f636b197
Matthew Yang <matthew.yang204@gmail.com>
2025-07-05 13:12:20 -0700

    Fix spelling error

--- Formula/dproc.rb
+++ Formula/dproc.rb
@@ -5,7 +5,7 @@ class Dproc < Formula
     if OS.mac?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-universal2-apple-darwin.tar.xz"
         sha256 "f41de0f1df46dc62afd837d5b500d052dc4315c220b3db776b7a7d1c6c93992d"
-    elseif OS.linux?
+    elsif OS.linux?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-x86_64-unknown-linux-gnu.tar.xz"
         sha256 "b2f1e6d2372102f9044676f65b8f6649db37c58614e32bd330dd26f5bc8c9937"
     end
