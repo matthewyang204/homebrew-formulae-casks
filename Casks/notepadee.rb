43373432929a154d425cc8c7684b5dcaae6b4ad6
Matthew Yang <matthew.yang204@gmail.com>
2025-08-02 10:32:48 -0700

    Bump version number to new recompile

--- Casks/notepadee.rb
+++ Casks/notepadee.rb
@@ -1,7 +1,7 @@
 cask "notepadee" do
   arch arm: "arm64", intel: "x86_64"
   
-  version "5.2.0"
+  version "5.2.0(2)"
   sha256 :no_check
 
   url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-#{arch}.zip"
