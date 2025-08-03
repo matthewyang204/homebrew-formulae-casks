9906c063ffe419d57471377655a58c6f56be2659
Matthew Yang <matthew.yang204@gmail.com>
2025-08-03 10:16:40 -0700

    Add libmysolvers formula

---
+++ Formula/libmysolvers.rb
@@ -0,0 +1,24 @@
+class Libmysolvers < Formula
+  desc "Matthew Yang's Equation Solving Library"
+  homepage "https://github.com/matthewyang204/libmysolvers"
+  head "https://github.com/matthewyang204/libmysolvers.git", branch: "main"
+  license "GPL-3.0"
+
+  depends_on "make" => :build
+  depends_on "gcc@15" => :build
+  depends_on "binutils" => :build
+  depends_on "gnu-sed" => :build
+
+  def install
+    ENV["CC"] = "gcc-15"
+    system "./configure", "--prefix=#{prefix}"
+    system "gmake"
+    system "gmake", "install"
+  end
+  
+  caveats <<~EOS
+  libmysolvers is a rolling-release library.
+  To update to the latest commit, run:
+    brew upgrade --fetch-HEAD libmysolvers
+  EOS
+end
