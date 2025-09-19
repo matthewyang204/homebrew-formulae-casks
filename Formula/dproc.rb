90c798383fd81f920d5e96cd38bf26305f07426f
Matthew Yang <matthew.yang204@gmail.com>
2025-09-19 15:32:27 -0700

    Update dproc SHA256

--- Formula/dproc.rb
+++ Formula/dproc.rb
@@ -4,10 +4,10 @@ class Dproc < Formula
     homepage "https://github.com/matthewyang204/dproc"
     if OS.mac?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-universal2-apple-darwin.tar.xz"
-        sha256 "2b37bdcf5c7668a674bb0278e538811a54f766a882f4a8fee83f7b1781687d32"
+        sha256 "c7d1af90a1955cb45e78abbf48cdef8df6ad57406043931dc3c05ddb807109cf"
     elsif OS.linux?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-x86_64-unknown-linux-gnu.tar.xz"
-        sha256 "345f2aef30a35e967cb0c638102ebce1807de357f1674080b5825495972eec48"
+        sha256 "0be6b00e2bab6ce97d0fe9767234d57fc77b4c7758dd51286acf7f003d68aab4"
     end
 
     def install
