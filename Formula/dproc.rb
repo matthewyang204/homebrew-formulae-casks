4a7a004f181b64637abff1e37a19abbb978d67a8
Matthew Yang <matthew.yang204@gmail.com>
2025-09-21 21:20:55 -0700

    Bump dproc to 1.2.2

--- Formula/dproc.rb
+++ Formula/dproc.rb
@@ -1,13 +1,13 @@
 class Dproc < Formula
-    version "1.2.1"
+    version "1.2.2"
     desc "A basic CLI data processor, designed to be fed data and output data directly from the commandline"
     homepage "https://github.com/matthewyang204/dproc"
     if OS.mac?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-universal2-apple-darwin.tar.xz"
-        sha256 "69949adb65bb347759ada71f7bc6e6f3f02033dbc6141a0e699a2f98cd32fea1"
+        sha256 "83c1ba1b670f4b47199513b69504658f1f62918b37724d9bc7b34e32e2d5d32b"
     elsif OS.linux?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-x86_64-unknown-linux-gnu.tar.xz"
-        sha256 "b3ffb4f867072efc74d84a4fa66a00cb69e2ddd0d21f7533ceaa73812310f6ee"
+        sha256 "2f20e68a7e0ef1c8ab603483ccd9818349a9cc569b578eebf528fec655f1dd49"
     end
 
     def install
