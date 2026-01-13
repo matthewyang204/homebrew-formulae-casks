de48de9b4d3ef5d1b091aadbac80757a2b2256eb
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2026-01-13 19:58:16 +0000

    Fix sha256

--- Formula/genignore.rb
+++ Formula/genignore.rb
@@ -3,7 +3,7 @@ class Genignore < Formula
   homepage "https://github.com/matthewyang204/genignore"
   url "https://github.com/matthewyang204/genignore/archive/refs/heads/production.zip"
   version "1.0.0"
-  sha256 "1e19fc804158a0941323c599cdbe30a0cf974015d63f9cd20866838a2432ae5d"
+  sha256 "a1d146fd418e2c31423d04061823c4f89445c2736c719c5a789707701d9ab7ba"
   license "GPL-3.0"
 
   depends_on "go" => :build
