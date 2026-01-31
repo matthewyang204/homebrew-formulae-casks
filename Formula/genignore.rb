0a4fa54aea57984ffd98b3e20acfc4b3b0c304c5
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-01-30 20:01:05 -0800

    Use brew audit to make formula genignore as correct as possible

--- Formula/genignore.rb
+++ Formula/genignore.rb
@@ -1,21 +1,20 @@
 class Genignore < Formula
-  desc "A utility for setting up .gitignores"
+  desc "Utility for setting up .gitignores"
   homepage "https://github.com/matthewyang204/genignore"
-  version "1.0.0"
-  url "https://github.com/regarager/genignore/archive/refs/tags/v#{version}.tar.gz"
+  url "https://github.com/regarager/genignore/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   depends_on "go" => :build
   depends_on "make" => :build
   depends_on "pandoc" => :build
-  
+
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "genignore", "--version"
+    system bin/"genignore", "--version"
   end
 end
