71f392aa217dd6109f62b7d2324ea6d7dfecb1ac
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-02-04 18:57:13 -0800

    Notepad== Revision 1 with Python 3.13.12

--- Casks/notepadee.rb
+++ Casks/notepadee.rb
@@ -2,9 +2,10 @@ cask "notepadee" do
   arch arm: "arm64", intel: "x86_64"
   
   version "5.2.4"
+  revision 1
   sha256 :no_check
 
-  url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-#{arch}.zip"
+  url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}_#{revision}/NotepadEE-macOS-#{arch}.zip"
   name "Notepad=="
   desc "Simple Windows Notepad replacement for Unix systems"
   homepage "https://github.com/matthewyang204/NotepadEE"
