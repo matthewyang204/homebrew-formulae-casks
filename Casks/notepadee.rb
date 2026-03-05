3373d7f4607d09b1756ae805f3019268d95f3fa4
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-03-05 11:24:12 -0800

    Bump Notepad== to 5.3.0

--- Casks/notepadee.rb
+++ Casks/notepadee.rb
@@ -1,7 +1,7 @@
 cask "notepadee" do
   arch arm: "arm64", intel: "x86_64"
   
-  version "5.2.4_1"
+  version "5.3.0"
   sha256 :no_check
 
   url "https://github.com/matthewyang204/NotepadEE/releases/download/#{version}/NotepadEE-macOS-#{arch}.zip"
