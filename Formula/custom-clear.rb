a93f0586c520b5b1c7bf942c136981f2549d40ce
Matthew Yang <matthew.yang204@gmail.com>
2026-04-11 07:23:19 -0700

    Bump custom-clear to 1.1

--- Formula/custom-clear.rb
+++ Formula/custom-clear.rb
@@ -1,9 +1,9 @@
 class CustomClear < Formula
   desc "My custom `clear` program for macOS, part of my collection of scripts"
   homepage "https://github.com/matthewyang204/scripts"
-  version "1.0.0"
-  url "https://github.com/matthewyang204/scripts/archive/refs/tags/clear-1.0.tar.gz"
-  sha256 "646979e5812e2d0a2d0214a77a519aac8586ce7164e05e7cc4ca80b370b85980"
+  version "1.1.0"
+  url "https://github.com/matthewyang204/scripts/archive/refs/tags/clear-1.1.tar.gz"
+  sha256 "6b2d18a387e617af24aee67346ce8c01942e936785a2fbcba192948f31fbe7d8"
   license "GPL-3.0"
 
   depends_on "bash"
@@ -11,7 +11,11 @@ class CustomClear < Formula
   def install
     system "mkdir -p build"
     system "cp misc/macos-clear build/clear"
-    system "cp misc/homebrew-clear.source build/clear.source"
+    if prefix.to_s.start_with?("/usr/local/Cellar")
+      system "cp misc/legacybrew-clear.source build/clear.source"
+    else
+      system "cp misc/homebrew-clear.source build/clear.source"
+    end
     bin.install "build/clear"
     share.install "build/clear.source"
   end
