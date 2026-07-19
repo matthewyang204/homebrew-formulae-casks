85cf01c9abd2b7d3ce0ce4fe8d50af59442b6ed6
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-07-19 10:36:22 -0700

    Update dproc formula version & add new multi-call binary

--- Formula/dproc.rb
+++ Formula/dproc.rb
@@ -1,13 +1,13 @@
 class Dproc < Formula
-    version "1.2.4"
+    version "1.3.0"
     desc "A basic CLI data processor, designed to be fed data and output data directly from the commandline"
     homepage "https://github.com/matthewyang204/dproc"
     if OS.mac?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-universal2-apple-darwin.tar.xz"
-        sha256 "d6a84a29feeb9eb93c77398e4c08dfc9d62b6790a32f746bbdde6b1b563dca11"
+        sha256 "60815169b5192616c550d962cb0b194e564fc0fdf56d8e51a57859c1deb68191"
     elsif OS.linux?
         url "https://github.com/matthewyang204/dproc/releases/download/#{version}/dproc-#{version}-x86_64-unknown-linux-gnu.tar.xz"
-        sha256 "be5d32637cfa2ce3390fbe72b6ec3388d5c4522413a85ea00f89e740a7011bea"
+        sha256 "bd333c1f5848bf36559bc169ab6f25815c6d653368511b3c777413cbad891a20"
     end
 
     def install
@@ -16,8 +16,10 @@ class Dproc < Formula
                 odie "dproc requires macOS Sierra or later to run."
             end
             bin.install "dproc"
+            bin.install "dfmtutils"
         elsif OS.linux?
             bin.install "bin/dproc"
+            bin.install "bin/dfmtutils"
         end
     end
 end
